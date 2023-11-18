2c89acfcb189a62617e4750914705cf5aa0fa4a8
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-18 11:54:58 +0300

    first commit|setting up the environment.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { faPlay, faTimes } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <>
-      <h1 className='text-3xl text-brightRed'>Hello World</h1>
+      <h1 className='text-3xl text-brightRed'>JitByte Website Clone</h1>
       <h1>
         <FontAwesomeIcon icon={faPlay} />
         <br />
