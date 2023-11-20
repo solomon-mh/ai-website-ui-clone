1e15831ffaba296a9c8495be7ee86654d2bdf274
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-20 17:14:22 +0300

    adding users data

---
+++ src/data/jibyteUsersData.js
@@ -0,0 +1,32 @@
+export const jibyteUsers = [
+  {
+    id: crypto.randomUUID(),
+    name: "Digital Agencies",
+    color: "#CBA153",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Product Designer",
+    color: "#AB7FE6",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Enterpreneurs",
+    color: "#57CBC6",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Copywriters",
+    color: "#7F8FE6",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Digital Marketers",
+    color: "#6BAC65",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Developers",
+    color: "#EF793A",
+  },
+];
