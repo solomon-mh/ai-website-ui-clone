e716c55aa25b7687cd79db30eda202003a280377
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-20 17:12:36 +0300

    adding AiTextGenerator section

---
+++ src/components/features-section/AiTextGenerator.jsx
@@ -0,0 +1,42 @@
+import NotificationHeader from "../../utils/NotificationHeader";
+import textGenerator from "../../assets/images/text-generator.png";
+const AiTextGenerator = () => {
+  return (
+    <div className='grid  grid-cols-2 gap-14 '>
+      <div className='left-side border-1 rounded-2xl p-8 bg-white'>
+        <div className='notf-btn text-black flex justify-start'>
+          <NotificationHeader
+            name="Say goodbye to writer's block"
+            content='AI'
+            bgColor={"var(--notification-btn-color)"}
+          />
+        </div>
+        <h1 className='text-4xl py-6'>Intelligent Writing Assistant</h1>
+        <p className='text-lg'>
+          Writer is designed to help you{" "}
+          <bold className='font-bold'>
+            generate high-quality texts instantly
+          </bold>
+          , without breaking a sweat. With our intuitive interface and powerful
+          features, you can easily edit, export or publish your AI-generated
+          result.
+        </p>
+      </div>
+      <div className='right-side text-center p-8 border-1 rounded-2xl bg-purple-200'>
+        <div className='box-border'>
+          <img
+            className='right-side-img w-full h-full transition-all duration-300 rounded-xl hover:scale-105'
+            src={textGenerator}
+            alt='text-generator-img'
+          />
+        </div>
+        <div className='pt-6 text-black'>
+          <b className='text-bold text-lg'>Generate, edit, export.</b>
+          <small className='text-xs block pt-1'>Powered by OpenAI.</small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AiTextGenerator;
