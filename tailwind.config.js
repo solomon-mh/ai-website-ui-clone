3429ec16ee710d208a79b3d7c301f17b67c57bcc
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-20 17:27:59 +0300

    final commit for the day

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,18 +15,26 @@ export default {
         "30perc": "30%",
         "90perc": "90%",
         17: "4.5rem",
+        120: "120%",
       },
       height: {
         17: "4.5rem",
       },
       fontSize: {
         "6.5xl": "4rem",
+        "7.5xl": "5rem",
       },
       transitionDuration: {
         2000: "2000ms",
       },
       borderWidth: {
         1: "1px",
+      },
+      borderRadius: {
+        "4xl": "3.5rem",
+      },
+      scale: {
+        200: "2.0",
       },
     },
   },
