ebb89f9a2d22e00579bb4086eb6aef11aee20a32
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-20 17:13:45 +0300

    adding features data

---
+++ src/data/features.js
@@ -0,0 +1,39 @@
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { faDashboard } from "@fortawesome/free-solid-svg-icons";
+import { faAd } from "@fortawesome/free-solid-svg-icons";
+import { faEquals } from "@fortawesome/free-solid-svg-icons";
+import { faPeopleLine } from "@fortawesome/free-solid-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+export const features = [
+  {
+    icon: faRobot,
+    name: "AI Gnerator",
+    content: "Generate text, image, code, chat and even more with",
+  },
+  {
+    icon: faDashboard,
+    name: "Advanced Dashboard",
+    content: "Access to valuable user insight, analytics and activity.",
+  },
+  {
+    icon: faAd,
+    name: "Payment Gateaways",
+    content: "Securely process credit card, debit card, or other methods.",
+  },
+  {
+    icon: faPeopleLine,
+    name: "Multi-Lingual",
+    content:
+      "Ability to understand and generate content in different languages",
+  },
+  {
+    icon: faEquals,
+    name: "Custom Templates",
+    content: "Add unlimited number of custom prompts for your customers.",
+  },
+  {
+    icon: faDatabase,
+    name: "Support Platform",
+    content: "Access and manage your support tickets from your dashboard.",
+  },
+];
