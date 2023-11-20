848a19631056056b72eb5e829afb582360fcbc38
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-20 17:20:38 +0300

    adding Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import FeaturesContainer from "../components/FeaturesContainer";
+import Hero from "../components/Hero";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+      <FeaturesContainer />
+    </div>
+  );
+};
+
+export default Home;
