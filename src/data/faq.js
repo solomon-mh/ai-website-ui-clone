872544fdf8c50d74e57718600045cdc1e3353548
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-21 16:37:18 +0300

    adding faqs section

---
+++ src/data/faq.js
@@ -0,0 +1,38 @@
+export const faqs = [
+  {
+    id: 1,
+    question: "How does it generate responses ?",
+    answer:
+      "Jibyte AI uses the most popular AI models such as GPT, Dall-E, Ada to create text, image, code and more within seconds. The process is simple. All you have to do is provide a topic or idea, and our AI-based generator will take care of the rest.",
+  },
+  {
+    id: 2,
+    question: "Can I create templates or chatbots ?",
+    answer:
+      "You can use pre-made templates and examples for various content types and industries to help you get started quickly. You can even create your own chatbot or custom prompt template for further customization.",
+  },
+  {
+    id: 3,
+    question: "Is there a mobile app for Jibyte AI ? ",
+    answer:
+      "Jibyte AI provides an almost native-app experience thanks to its mobile-first approach. The entire layout is responsive and works great on any device regardless of the size.",
+  },
+  {
+    id: 4,
+    question: "What is Jibyte ?",
+    answer:
+      "Discover endless possibilities with Jibyte. Our versatile platform combines AI-powered features including fitness training, content writing, AI image generation, code generation, paraphrasing, and translation and others. Access experts in over 100 fields to enhance your projects, create unique content, and elevate your online presence.",
+  },
+  {
+    id: 5,
+    question: "Is Jibyte Free ?",
+    answer:
+      "Yes, Jibyte offers a free subscription with 2,500 words and 10 images. Our free tools allow you to create content. We offer a paid subscription plan if you wish to access more content.",
+  },
+  {
+    id: 6,
+    question: "How can Jibyte help for developers ?",
+    answer:
+      "With Jibyte, developers can generate code using specific programming languages and receive explanations.",
+  },
+];
