149116fde3253ec62eb6b5c751f276bd92e5a6ac
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-21 12:16:16 +0300

    modfying Home section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
+import FAQ from "../components/FAQ";
 import FeaturesContainer from "../components/FeaturesContainer";
+import HalfMoon from "../components/HalfMoon";
 import Hero from "../components/Hero";
+import HowItWorks from "../components/HowItWorks";
+import LatestBlogs from "../components/LatestBlogs";
 import Navigation from "../components/Navigation";
+import Pricing from "../components/Pricing";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
@@ -8,6 +14,12 @@ const Home = () => {
       <Navigation />
       <Hero />
       <FeaturesContainer />
+      <HowItWorks />
+      <Testimonials />
+      <Pricing />
+      <FAQ />
+      <LatestBlogs />
+      <HalfMoon />
     </div>
   );
 };
