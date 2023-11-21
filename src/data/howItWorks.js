367c0fff42629850a3f0abb3c5e3edf817880822
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-21 12:12:10 +0300

    adding some data section

---
+++ src/data/howItWorks.js
@@ -0,0 +1,17 @@
+export const howItWorks = [
+  {
+    id: 1,
+    content:
+      "Simply explain what your content is about and adjust settings according to your needs.",
+  },
+  {
+    id: 2,
+    content:
+      "Simply input some basic information or keywords about your brand or product, and let our AI algorithms do the rest.",
+  },
+  {
+    id: 3,
+    content:
+      "View, edit or export your result with a few clicks. And you’re done!",
+  },
+];
