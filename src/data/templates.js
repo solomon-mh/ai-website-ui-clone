e145a9763a25d16740a4bbce2f2c571869e71732
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-21 12:13:39 +0300

    adding some data section

---
+++ src/data/templates.js
@@ -0,0 +1,312 @@
+import {
+  faMailBulk,
+  faMessage,
+  faAddressBook,
+  faArchive,
+  faBagShopping,
+  faBitcoinSign,
+  faCalendar,
+  faDatabase,
+  faDashboard,
+  faKeyboard,
+  faImage,
+  faPodcast,
+  faNoteSticky,
+  faBlog,
+  faPerson,
+  faPlay,
+  faRetweet,
+} from "@fortawesome/free-solid-svg-icons";
+export const templates = [
+  {
+    id: crypto.randomUUID(),
+    title: "Post Title Generator",
+    icon: faMessage,
+    type: "blog",
+    content:
+      "Get captivating post titles instantly with our title generator. Boost engagement and save time.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Summarize Text",
+    icon: faAddressBook,
+    type: "blog",
+    content:
+      "Effortlessly condense large text into shorter summaries. Save time and increase productivity.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Product Description",
+    icon: faPodcast,
+    type: "ecommerce",
+    content:
+      "  Easily create compelling product descriptions that sell. Increase conversions and boost sales.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Article Generator",
+    icon: faDashboard,
+    type: "blog",
+    content:
+      "    Instantly create unique articles on any topic. Boost engagement, improve SEO, and save time.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Product Name Generator    ",
+    icon: faBagShopping,
+    type: "ecommerce",
+    content:
+      "Create catchy product names with ease. Attract customers and boost sales effortlessly.   ",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Testimonial Review",
+    icon: faKeyboard,
+    type: "ecommerce",
+    content:
+      "Instantly generate authentic testimonials. Build trust and credibility with genuine reviews.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Problem Agilate Solution",
+    icon: faCalendar,
+    type: "blog",
+    content:
+      "Identify and solve problems efficiently. Streamline solutions and increase productivity.    ",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Blog Section",
+    icon: faArchive,
+    type: "blog",
+    content:
+      "Effortlessly create blog sections with AI. Get unique, engaging content and save time.",
+  },
+  {
+    id: crypto.randomUUID(),
+
+    title: "Blog Post Ideas",
+    icon: faBlog,
+    type: "blog",
+    content:
+      "Unlock your creativity with unique blog post ideas. Generate endless inspiration and take your content to the next level.",
+  },
+  {
+    id: crypto.randomUUID(),
+
+    title: "Blog Intros",
+    icon: faBitcoinSign,
+    type: "blog",
+    content:
+      "Set the tone for your blog post with captivating intros. Grab readers' attention and keep them engaged.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Blog Conclusion",
+    icon: faArchive,
+    type: "blog",
+    content:
+      "End your blog posts on a high note. Craft memorable conclusions that leave a lasting impact.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Facebook Ads",
+    icon: faPodcast,
+    type: "advertisement",
+    content:
+      "Create high-converting Facebook ads that grab attention. Drive sales and grow your business.",
+  },
+  {
+    id: crypto.randomUUID(),
+
+    title: "Youtube Video Description",
+    icon: faPlay,
+    type: "socialmedia",
+    content:
+      "Elevate your YouTube content with compelling video descriptions. Generate engaging descriptions effortlessly and increase views.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Youtube Video Title",
+    icon: faPlay,
+    type: "socialmedia",
+    content:
+      "Get more views with attention-grabbing video titles. Create unique, catchy titles that entice viewers.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Youtube Video Tag",
+    icon: faPlay,
+    type: "socialmedia",
+    content:
+      "Improve your YouTube video's discoverability with relevant video tags. Boost views and engagement.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Instagram Captions",
+    icon: faNoteSticky,
+    type: "socialmedia",
+    content:
+      "Elevate your Instagram game with captivating captions. Generate unique captions that engage followers and increase your reach.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Instagram Hashtags",
+    icon: faNoteSticky,
+    type: "socialmedia",
+    content:
+      "Boost your Instagram reach with relevant hashtags. Generate optimal, trending hashtags and increase your visibility.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Social Media Post Tweet",
+    icon: faRetweet,
+    type: "socialmedia",
+    content:
+      "Make an impact with every tweet. Generate attention-grabbing social media posts and increase engagement.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Social Media Post Business",
+    icon: faBagShopping,
+    type: "socialmedia",
+    content:
+      "Generate a text for your business social media networks. Maximize your social media presence with impactful business posts.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Facebook Headlines",
+    icon: faBlog,
+    type: "socialmedia",
+    content:
+      "Get noticed with attention-grabbing Facebook headlines. Generate unique, clickable headlines that increase engagement and drive traffic.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Google Ads Headlines",
+    icon: faNoteSticky,
+    type: "advertisement",
+    content:
+      "Create high-converting Google ads with captivating headlines. Generate unique, clickable ads that drive traffic and boost sales.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Google Ads Description",
+    icon: faDatabase,
+    type: "advertisement",
+    content:
+      "Step up your Google ad game, Craft high-converting ad copy that grabs attention and drives sales.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Paragraph Generator",
+    icon: faArchive,
+    type: "blog",
+    content:
+      "Generate a paragraph with keywords and description. Never struggle with writer's block again. Generate flawless paragraphs that captivate readers.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Pros & Cons",
+    icon: faNoteSticky,
+    type: "development",
+    content:
+      "Make informed decisions with ease. Generate unbiased pros and cons lists that help you weigh options and make better choices.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Meta Description",
+    icon: faImage,
+    type: "development",
+    content:
+      "Get more clicks with compelling meta descriptions. Generate unique, SEO-friendly meta descriptions that attract customers and boost traffic.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "FAQ Generator (All Datas)",
+    icon: faPerson,
+    type: "development",
+    content:
+      "Quickly create helpful FAQs. Our AI-powered generator provides custom responses to common questions in seconds.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Email Generator",
+    icon: faMailBulk,
+    type: "blog",
+    content:
+      "Generate an email with your subject and description. Streamline your inbox and save time.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Email Answer Generator",
+    icon: faCalendar,
+    type: "blog",
+    content:
+      "Effortlessly tackle your overflowing inbox with custom, accurate responses to common queries, freeing you up to focus on what matters most.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Newsletter Generator",
+    icon: faNoteSticky,
+    type: "blog",
+    content:
+      "Generate engaging newsletters easily with personalized content that resonates with your audience, driving growth and engagement.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Grammar Correction",
+    icon: faNoteSticky,
+    type: "blog",
+    content:
+      "Eliminate grammar errors and enhance your writing with ease. Our tool offers seamless grammar correction for flawless content.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "TL;DR Summarization",
+    icon: faBagShopping,
+    type: "blog",
+    content:
+      "Automatically summarize long texts into bite-sized summaries with this TL;DR generator.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "AI Image Generator",
+    icon: faPerson,
+    type: "development",
+    content:
+      "Unleash your creativity with our AI image generator that produces stunning visuals in seconds.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "AI Speech to Text",
+    icon: faImage,
+    type: "blog",
+    content:
+      "The AI app that turns audio speech into text with ease. Get ready to generate custom texts from audio files quickly and accurately.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "AI Code Generator",
+    icon: faBitcoinSign,
+    type: "development",
+    content:
+      "Create custom code in seconds! Leverage our state-of-the-art AI technology to quickly and easily generate code in any language.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "AI Voiceover",
+    icon: faDatabase,
+    type: "voiceover",
+    content:
+      "The AI app that turns audio speech into text with ease. Get ready to generate custom texts from audio files quickly and accurately.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "AI Article Wizard Generator",
+    icon: faAddressBook,
+    type: "blog",
+    content:
+      "Create custom article instantly with our article wizard generator. Boost engagement and save time.",
+  },
+];
