25f4e13e6677289cc7a5966c353890780ffa831d
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-21 12:19:00 +0300

    modfying utils section

--- src/utils/JiByteUsersBox.jsx
+++ src/utils/JiByteUsersBox.jsx
@@ -1,24 +1,15 @@
 /* eslint-disable react/prop-types */
 const JiByteUsersBox = ({ name, color }) => {
-  const handleHover = (event) => {
-    event.currentTarget.style.boxShadow = `0 10px rgba(${color}, 0.5)`;
-  };
-
-  const handleHoverOut = (event) => {
-    event.currentTarget.style.boxShadow = ""; // Reset boxShadow on hover out
-  };
-
   const fadedColor = `rgba(${parseInt(color.slice(-6, -4), 16)}, ${parseInt(
     color.slice(-4, -2),
     16
-  )}, ${parseInt(color.slice(-2), 16)}, 0.1)`; // Adjust alpha value (0.1 for example)
-
+  )}, ${parseInt(color.slice(-2), 16)}, 0.1)`;
   return (
     <div
-      onMouseOver={handleHover}
-      onMouseOut={handleHoverOut}
-      style={{ background: fadedColor }} // Apply fadedColor
-      className={`user-box flex justify-center items-center border-1 py-5 rounded-lg bg-${color} transition-all duration-300  hover:-translate-y-2`}
+      style={{
+        background: fadedColor,
+      }}
+      className='user-box flex justify-center items-center border-1 border-slate-50 py-5 rounded-lg transition-all duration-300  hover:-translate-y-2'
     >
       <div className='circle bg-white p-2 rounded-full'>
         <span
@@ -26,11 +17,56 @@ const JiByteUsersBox = ({ name, color }) => {
           className='block w-3 h-3  rounded-full '
         ></span>
       </div>
-      <div style={{ color: color }} className='user'>
-        <h2 className='text-xl font-bold px-6'>{name}</h2>
+      <div className='user'>
+        <h2 style={{ color: color }} className='text-xl font-bold px-6'>
+          {name}
+        </h2>
       </div>
     </div>
   );
 };
 
 export default JiByteUsersBox;
+// /* eslint-disable react/prop-types */
+// const JiByteUsersBox = ({ name, color }) => {
+//   const fadedColor = `rgba(${parseInt(color.slice(-6), 16)}, ${parseInt(
+//     color.slice(-4, -2),
+//     16
+//   )}, ${parseInt(color.slice(-2), 16)}, 0.1)`; // Adjust alpha value (0.1 for example)
+
+//
+
+//   return (
+//     <div
+//       onMouseOver={handleHover}
+//       onMouseOut={handleHoverOut}
+//       style={{ background: fadedColor }} // Apply fadedColor
+//       className={`user-box flex justify-center items-center border-1 py-5 rounded-lg bg-${color} transition-all duration-300 hover:-translate-y-2`}
+//     >
+//       <div className='circle bg-white p-2 rounded-full'>
+//         <span
+//           style={{ background: color }}
+//           className='block w-3 h-3 rounded-full'
+//         ></span>
+//       </div>
+//       <div style={{ color }} className='user'>
+//         <h2 className='text-xl font-bold px-6'>{name}</h2>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default JiByteUsersBox;
+// const handleHover = (event) => {
+//   event.currentTarget.style.boxShadow = `0 10px 22px  rgba(${parseInt(
+//     color.slice(-6 - 4),
+//     16
+//   )}, ${parseInt(color.slice(-4, -2), 16)}, ${parseInt(
+//     color.slice(-2),
+//     16
+//   )}, 0.1)`;
+// };
+
+// const handleHoverOut = (event) => {
+//   event.currentTarget.style.boxShadow = ""; // Reset boxShadow on hover out
+// };
