cbff85f4e0da11a7812c31abbdc114bde9a13c0a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-21 12:19:29 +0300

    modfying utils section

--- src/utils/NotificationHeader.jsx
+++ src/utils/NotificationHeader.jsx
@@ -3,16 +3,27 @@ const NotificationHeader = ({
   name,
   content,
   bgColor = "var(--default-notification-btn-color)",
+  textColor = "var(--notification-default-text-color)",
 }) => {
   return (
     <div className='py-4'>
       <h6
         style={{ background: bgColor }}
-        className='w-fit text-xs py-1 my-4 mx-auto flex items-center  rounded-full px-4'
+        className='w-fit text-xs py-1 my-4 mx-auto flex items-center rounded-md px-4'
       >
-        <span>{name}</span>{" "}
+        <span
+          style={{ color: textColor }}
+          className='text-blue-900 font-semibold'
+        >
+          {name}
+        </span>
         <span className='mx-2 border-2 rounded border-blue-900'></span>
-        <span className='opacity-60'> {content}</span>
+        <span
+          style={{ color: textColor }}
+          className='text-blue-800 font-semibold opacity-60'
+        >
+          {content}
+        </span>
       </h6>
     </div>
   );
