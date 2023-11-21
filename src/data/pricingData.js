c4863ee0e76bab1f660e42e549424f822234a4ee
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-21 12:13:12 +0300

    adding some data section

---
+++ src/data/pricingData.js
@@ -0,0 +1,44 @@
+export const pricingData = [
+  {
+    id: crypto.randomUUID(),
+    type: "Premium",
+    price: "30.99",
+    resources: [
+      "100000 Word Limit",
+      "1000 Image Limit",
+      "Chat-GPT 4.0",
+      "Premium Templates",
+      "AI Code",
+      "VIP Support",
+      "100,000 Word Tokens",
+      "100 Image Tokens",
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    price: "5.99",
+    type: "Standard",
+    resources: [
+      "10000 Word Limit",
+      "20 Image Limit",
+      "ChatGPT 3.5",
+      "Standard Templates",
+      "10,000 Word Tokens",
+      "15 Image Tokens",
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    price: "10.99",
+    type: "Pro",
+    resources: [
+      "25000 Word Limit",
+      "250 Image Limit",
+      "ChatGPT 4.0",
+      "Premium Templates",
+      "Basic Support",
+      "25,000 Word Tokens",
+      "50 Image Tokens",
+    ],
+  },
+];
