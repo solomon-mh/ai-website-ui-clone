4578d849cbf1f90c597f241cc58ea1eef2b9000a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-24 15:35:58 +0300

    adding testimonial section data

---
+++ src/data/testimonial.js
@@ -0,0 +1,30 @@
+import userOne from "../assets/testimonial-img/user1.jpg";
+import userTwo from "../assets/testimonial-img/user2.jpg";
+import userThree from "../assets/testimonial-img/user3.png";
+
+export const testimonial = [
+  {
+    id: 1,
+    img: userTwo,
+    name: "Dave Minching",
+    profession: "Backend Developer",
+    testimony:
+      "I love using Jibyte it really is a great help for my work.It makes my life much and much simpler and easier than before. I really thank you,Jibye.",
+  },
+  {
+    id: 2,
+    img: userOne,
+    name: "Peline Jan",
+    profession: "Entrepreneur",
+    testimony:
+      "I love using Jibyte it really is a great help for my work.It makes my life much and much simpler and easier than before. I really thank you,Jibye.",
+  },
+  {
+    id: 3,
+    img: userThree,
+    name: "Miguel Alarez",
+    profession: "Graphic Designer.",
+    testimony:
+      "I love using Jibyte it really is a great help for my work.It makes my life much and much simpler and easier than before. I really thank you,Jibye.",
+  },
+];
