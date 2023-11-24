c061b3539e7a3a7918643b597dbd028e55e9da55
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-24 15:55:36 +0300

    working on responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBoltLightning } from "@fortawesome/free-solid-svg-icons";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
 import NotificationHeader from "../utils/NotificationHeader";
 import "../styles/component-styling.css";
 import { useEffect, useState } from "react";
@@ -23,13 +23,14 @@ const Hero = () => {
         name='JiByteAI'
         content='Unleash the power of AI'
         textColor='white'
+        bgColor='var(--lighter-blue-notification-btn-color)'
       />
-      <div className=' w-3/5 m-auto px-12'>
-        <header>
+      <div className='md:w-3/5 m-auto px-12'>
+        <header className='md:w-1/2 m-auto'>
           <h1 className='text-white text-7.5xl transition-all duration-700'>
             Ultimate AI {functionalities[currentIndex]}
             <FontAwesomeIcon
-              icon={faBoltLightning}
+              icon={faBolt}
               style={{ fontSize: "4rem", marginLeft: "12px" }}
             />
           </h1>
@@ -43,10 +44,7 @@ const Hero = () => {
         <div className='hero-button'>
           <button className='font-sans font-bold my-6 py-4 px-8 rounded-xl transition-all duration-500 bg-slate-800 hover:bg-black hover:scale-105'>
             <span>
-              <FontAwesomeIcon
-                icon={faBoltLightning}
-                style={{ marginRight: "10px" }}
-              />
+              <FontAwesomeIcon icon={faBolt} style={{ marginRight: "10px" }} />
             </span>
             Start Making Money.
           </button>
