0489dae13ecf62bb1ffba5bb0eff3b53b5d0719a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-24 16:01:22 +0300

    working on responsiveness

--- src/components/features-section/AiSpeechToText.jsx
+++ src/components/features-section/AiSpeechToText.jsx
@@ -2,7 +2,7 @@ import NotificationHeader from "../../utils/NotificationHeader";
 import speechToTextImage from "../../assets/images/ai-speech.png";
 const AiSpeechToText = () => {
   return (
-    <div className='grid  grid-cols-2 gap-14 '>
+    <div className='grid md:grid-cols-2 gap-14 '>
       <div className='left-side border-1 rounded-2xl p-8 bg-white'>
         <div className='notf-btn text-black flex justify-start'>
           <NotificationHeader
