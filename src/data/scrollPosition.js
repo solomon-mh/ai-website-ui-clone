cc40051f1eca46c6b1ddb876ce10e372ce568577
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-24 15:34:40 +0300

    adding scroll position data

---
+++ src/data/scrollPosition.js
@@ -0,0 +1,16 @@
+export const homePosition = {
+  min: 0,
+  max: 640,
+};
+export const FeaturesPosition = {
+  min: 643,
+  max: 4100,
+};
+export const HowItWorksPosition = {
+  min: 4105,
+  max: 4870,
+};
+export const TesttimonialPosition = {
+  min: 4106,
+  max: 5725,
+};
