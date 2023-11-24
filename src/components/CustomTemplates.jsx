fec586848e53f770b80c63c36592a6ea1d12f9f2
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-24 15:46:46 +0300

    working on responsiveness

--- src/components/CustomTemplates.jsx
+++ src/components/CustomTemplates.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 import { useSearchParams } from "react-router-dom";
 import NotificationHeader from "../utils/NotificationHeader";
 import { templates } from "../data/templates";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ShowMoreCards from "../utils/ShowMoreCards";
 const CustomTemplates = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const typeFilter = searchParams.get("type");
@@ -10,23 +11,7 @@ const CustomTemplates = () => {
         (template) => template.type?.toLowerCase() === typeFilter
       )
     : templates;
-  console.log(filteredTemplate);
-  const temps = filteredTemplate.map((template) => {
-    return (
-      <div
-        key={template.id}
-        className='template-card border-1 pt-8 pb-2 rounded-xl px-4 text-left bg-slate-200 bg-opacity-40'
-      >
-        <div className='template-icon text-xl py-2 mx-6 px-4 border-1 w-fit bg-gray-300 rounded-lg transition-all duration-300'>
-          <FontAwesomeIcon icon={template.icon} />
-        </div>
-        <h2 className='text-xl font-semibold text-black py-2'>
-          {template.title}
-        </h2>
-        <p className='text-sm pb-2'>{template.content}</p>
-      </div>
-    );
-  });
+
   return (
     <div>
       <div className='btn'>
@@ -37,22 +22,26 @@ const CustomTemplates = () => {
           textColor='navy'
         />
       </div>
-      <div className='content w-3/4 m-auto px-10 py-8 text-justify'>
-        <h1 className='text-5xl py-5 text-center'>Custom Templates.</h1>
+      <div className='content md:w-3/4 m-auto  py-8 text-center'>
+        <h1 className='text-4xl md:text-5xl py-5 text-center'>
+          Custom Templates.
+        </h1>
         <p className='text-lg font-semibold'>
           To get started quickly, create your own template or utilize pre-made
           templates and samples for various content categories and sectors.
         </p>
       </div>
-      <div className='template-btns text-sm w-4/5 m-auto border-1 py-2 px-3 flex justify-around rounded-xl'>
+      <div className='template-btns text-sm w-full md:w-90perc m-auto border-1 py-2 flex flex-wrap justify-center rounded-xl'>
         <button
-          className={`${typeFilter === "" ? "selected-template-btn" : ""}`}
+          className={`${typeFilter === "" ? "selected-template-btn" : ""}
+          px-2.5 py-1 rounded`}
           onClick={() => setSearchParams({ type: "" })}
         >
           All
         </button>
         <button
-          className={`${typeFilter === "blog" ? "selected-template-btn" : ""}`}
+          className={`${typeFilter === "blog" ? "selected-template-btn" : ""} 
+          px-2.5 py-1 rounded`}
           onClick={() => setSearchParams({ type: "blog" })}
         >
           Blog
@@ -60,7 +49,8 @@ const CustomTemplates = () => {
         <button
           className={`${
             typeFilter === "ecommerce" ? "selected-template-btn" : ""
-          }`}
+          }
+          px-2.5 py-1 rounded`}
           onClick={() => setSearchParams({ type: "ecommerce" })}
         >
           Ecommerce
@@ -68,7 +58,8 @@ const CustomTemplates = () => {
         <button
           className={`${
             typeFilter === "development" ? "selected-template-btn" : ""
-          }`}
+          }
+          px-2.5 py-1 rounded`}
           onClick={() => setSearchParams({ type: "development" })}
         >
           Development
@@ -76,18 +67,18 @@ const CustomTemplates = () => {
         <button
           className={`${
             typeFilter === "advertisement" ? "selected-template-btn" : ""
-          }`}
+          }
+          px-2.5 py-1 rounded`}
           onClick={() => setSearchParams({ type: "advertisement" })}
         >
           Advertisement
         </button>
-        <button disabled={true} onClick={() => setSearchParams({ type: "" })}>
-          Custom
-        </button>
+        <button disabled={true}>Custom</button>
         <button
           className={`${
             typeFilter === "socialmedia" ? "selected-template-btn" : ""
-          }`}
+          }
+          px-2.5 py-1 rounded`}
           onClick={() => setSearchParams({ type: "socialmedia" })}
         >
           Social Media
@@ -95,13 +86,16 @@ const CustomTemplates = () => {
         <button
           className={`${
             typeFilter === "voiceover" ? "selected-template-btn" : ""
-          }`}
+          }
+          px-2.5 py-1 rounded`}
           onClick={() => setSearchParams({ type: "voiceover" })}
         >
           Voiceover
         </button>
       </div>
-      <div className='grid grid-cols-3 gap-4 my-8'>{temps}</div>
+      <section>
+        <ShowMoreCards filteredTemplate={filteredTemplate} />
+      </section>
     </div>
   );
 };
