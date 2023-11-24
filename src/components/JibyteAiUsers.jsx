f062bdcd46adc70a3dea472d5766a03aac1a7d76
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-24 15:58:15 +0300

    working on responsiveness

--- src/components/JibyteAiUsers.jsx
+++ src/components/JibyteAiUsers.jsx
@@ -5,7 +5,7 @@ const JibyteAiUsers = () => {
   const indUsers = jibyteUsers.map((user) => (
     <JiByteUsersBox key={user.id} name={user.name} color={user.color} />
   ));
-  return <div className='grid  md:grid-cols-3 gap-4'>{indUsers}</div>;
+  return <div className='grid md:grid-cols-3 gap-4 my-12'>{indUsers}</div>;
 };
 
 export default JibyteAiUsers;
