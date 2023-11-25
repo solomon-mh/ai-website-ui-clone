cbf580e7633c9d4579c1207592b07f8764950448
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-11-25 10:00:27 +0300

    working on responsiveness

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -7,18 +7,21 @@ const HowItWorks = () => {
         <div className='how-it-work-number transition-all duration-300 text-4xl p-2 border-1 border-slate-600 rounded-full w-fit m-auto px-5 flex items-center my-12'>
           {item.id}
         </div>
-        <div>
+        <div className='m-auto px-8 sm:w-4/5 md:w-full md:px-0'>
           <p className='text-white opacity-60 text-xl'>{item.content}</p>
         </div>
       </div>
     );
   });
   return (
-    <section className='text-center bg-black w-90perc m-auto my-20 pb-12 pt-10 px-12 border-2 rounded-4xl'>
-      <h1 className='w-3/5 opacity-80 text-white m-auto text-6xl pt-12'>
+    <section
+      id='HowItWorks'
+      className='text-center mx-6 bg-black sm:m-auto my-20 pb-12 pt-10 border-2 rounded-4xl sm:w-4/5 lg:w-90perc'
+    >
+      <h1 className='md:w-3/5 text-white m-auto text-5xl sm:text-6xl pt-12'>
         So, how does it work?
       </h1>
-      <div className=' grid px-12 md:grid-cols-3 gap-3 my-12'>{items}</div>
+      <div className='grid px-12 md:grid-cols-3 gap-3 my-12'>{items}</div>
       <div className='join-link'>
         <a className='text-blue-300 opacity-60' href='#'>
           Want to see it? Join Jibyte
