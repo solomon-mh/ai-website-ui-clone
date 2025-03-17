0a6076f0fc9c9e5f0ffd2a4f90fb314bd43405af
solomon-mh <solomon.muhye.wd@gmail.com>
2025-03-17 12:26:07 +0300

    refactor App component for improved code readability and consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-//   faPlay,
-//   faTimes,
-//   faBoltLightning,
-// } from "@fortawesome/free-solid-svg-icons";
 import Home from "./pages/Home";
 import AiTextGenerator from "./components/features-section/AiTextGenerator";
 import AiImageGenerator from "./components/features-section/AiImageGenerator";
@@ -13,21 +7,21 @@ import AiSpeechToText from "./components/features-section/AiSpeechToText";
 import AiCodeGenerator from "./components/features-section/AiCodeGenerator";
 // import FeaturesContainer from "./components/FeaturesContainer";
 function App() {
-  return (
-    <div className='App overflow-hidden'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />}>
-            <Route index element={<AiTextGenerator />} />
-            <Route path='/aiimagegenerator' element={<AiImageGenerator />} />
-            <Route path='/aichatbot' element={<AiChatbot />} />
-            <Route path='/aispeechtotext' element={<AiSpeechToText />} />
-            <Route path='/aicodegenerator' element={<AiCodeGenerator />} />
-          </Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App overflow-hidden">
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />}>
+						<Route index element={<AiTextGenerator />} />
+						<Route path="/aiimagegenerator" element={<AiImageGenerator />} />
+						<Route path="/aichatbot" element={<AiChatbot />} />
+						<Route path="/aispeechtotext" element={<AiSpeechToText />} />
+						<Route path="/aicodegenerator" element={<AiCodeGenerator />} />
+					</Route>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
