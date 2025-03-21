c6e799441378e735332911907e6004b191915544
solomon-mh <solomon.muhye.wd@gmail.com>
2025-03-21 16:52:14 +0300

    refactor FAQ component for improved code readability and consistency

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -2,48 +2,48 @@ import { useState } from "react";
 import NotificationHeader from "../utils/NotificationHeader";
 import { faqs } from "../data/faq";
 const FAQs = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
+	const [activeIndex, setActiveIndex] = useState(null);
 
-  const toggleAnswer = (index) => {
-    setActiveIndex(activeIndex === index ? null : index);
-  };
-  return (
-    <section
-      id='FAQ'
-      className='text-center w-90perc m-auto my-20 pb-12 pt-10 px-4 md:px-10 border-2 rounded-4xl'
-    >
-      <NotificationHeader name='FAQ' content='Help Center' textColor='purple' />
-      <div className='py-4 md:w-3/4 m-auto px-8'>
-        <h1 className='text-2xl md:text-5xl py-6'>Have a question?</h1>
-        <p className='text-sm md:text-xl'>
-          Our support team will get assistance from AI-powered suggestions,
-          making it quicker than ever to handle support requests.
-        </p>
-      </div>
-      <div>
-        {faqs.map((faq, index) => (
-          <div key={faq.id} className='faq-item cursor-pointer md:mx-24'>
-            <div
-              className='faq-question my-4 py-4 pb-6 border-b-2 flex justify-between'
-              onClick={() => toggleAnswer(index)}
-            >
-              <p className='text-black sm:text-xl hover:cursor-pointer'>
-                {faq.question}
-              </p>
-              <span className='block cursor-pointer w-8 h-8 border-1 border-gray-400 bg-white rounded-full'>
-                {activeIndex === index ? "-" : "+"}
-              </span>
-            </div>
-            {activeIndex === index && (
-              <div className='faq-answer text-xs sm:text-base text-left'>
-                {faq.answer}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+	const toggleAnswer = (index) => {
+		setActiveIndex(activeIndex === index ? null : index);
+	};
+	return (
+		<section
+			id="FAQ"
+			className="text-center w-90perc m-auto my-20 pb-12 pt-10 px-4 md:px-10 border-2 rounded-4xl"
+		>
+			<NotificationHeader name="FAQ" content="Help Center" textColor="purple" />
+			<div className="py-4 md:w-3/4 m-auto px-8">
+				<h1 className="text-2xl md:text-5xl py-6">Have a question?</h1>
+				<p className="text-sm md:text-xl">
+					Our support team will get assistance from AI-powered suggestions,
+					making it quicker than ever to handle support requests.
+				</p>
+			</div>
+			<div>
+				{faqs.map((faq, index) => (
+					<div key={faq.id} className="faq-item cursor-pointer md:mx-24">
+						<div
+							className="faq-question my-4 py-4 pb-6 border-b-2 flex justify-between"
+							onClick={() => toggleAnswer(index)}
+						>
+							<p className="text-black sm:text-xl hover:cursor-pointer">
+								{faq.question}
+							</p>
+							<span className="flex justify-center items-center font-bold cursor-pointer w-8 h-8 border-1 border-gray-400 bg-white rounded-full">
+								{activeIndex === index ? "-" : "+"}
+							</span>
+						</div>
+						{activeIndex === index && (
+							<div className="faq-answer text-xs sm:text-base text-left">
+								{faq.answer}
+							</div>
+						)}
+					</div>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default FAQs;
