a1530fbe4a6d2eeabf16dc03af6972e3525db3ad
solomon-mh <solomon.muhye.wd@gmail.com>
2025-03-21 16:53:07 +0300

    refactor ShowMoreCards component for improved code readability and styling consistency

--- src/utils/ShowMoreCards.jsx
+++ src/utils/ShowMoreCards.jsx
@@ -3,41 +3,41 @@ import { useState } from "react";
 import { templates } from "../data/templates";
 import TemplateCards from "../components/TemplateCards";
 const ShowMoreCards = ({ filteredTemplate }) => {
-  const initialVisibleCards = 6;
-  const [showAll, setShowAll] = useState(false);
-  const [visibleCards, sertVisibleCards] = useState(initialVisibleCards);
-  const handleShowMore = () => {
-    if (showAll) {
-      sertVisibleCards(initialVisibleCards);
-    } else {
-      sertVisibleCards(templates.length);
-    }
-    setShowAll((prev) => !prev);
-  };
+	const initialVisibleCards = 6;
+	const [showAll, setShowAll] = useState(false);
+	const [visibleCards, sertVisibleCards] = useState(initialVisibleCards);
+	const handleShowMore = () => {
+		if (showAll) {
+			sertVisibleCards(initialVisibleCards);
+		} else {
+			sertVisibleCards(templates.length);
+		}
+		setShowAll((prev) => !prev);
+	};
 
-  return (
-    <>
-      <div
-        className={`${!showAll ? "opacity-50" : "opacity-100"}
-        card-container cursor-pointer grid md:grid-cols-2 lg:grid-cols-3 gap-4 my-8 transition-all duration-300`}
-      >
-        {filteredTemplate.slice(0, visibleCards).map((card, index) => (
-          <TemplateCards key={index} cardContent={card} />
-        ))}
-      </div>
-      <div className='text-center'>
-        <span className='mx-2 bg-purple-100 px-2 py-1.5 rounded-lg text-purple-800'>
-          {showAll ? "-" : "+"}
-        </span>
-        <button
-          onClick={handleShowMore}
-          className='show-more-btn my-12  p-1.5 text-sm font-semibold rounded-lg bg-purple-100 text-purple-800'
-        >
-          {showAll ? "Show Less" : "Show More"}
-        </button>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className={`${!showAll ? "opacity-60" : "opacity-100"}
+        card-container overflow-hidden cursor-pointer grid md:grid-cols-2 lg:grid-cols-3 gap-4 my-8 transition-all duration-300`}
+			>
+				{filteredTemplate.slice(0, visibleCards).map((card, index) => (
+					<TemplateCards key={index} cardContent={card} />
+				))}
+			</div>
+			<div className="text-center">
+				<span className="mx-2 bg-purple-100 px-2 py-1.5 rounded-lg text-purple-800">
+					{showAll ? "-" : "+"}
+				</span>
+				<button
+					onClick={handleShowMore}
+					className="show-more-btn my-12  p-1.5 text-sm font-semibold rounded-lg bg-purple-100 text-purple-800"
+				>
+					{showAll ? "Show Less" : "Show More"}
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default ShowMoreCards;
